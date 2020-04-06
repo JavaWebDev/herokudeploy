b8fa69ddd106554fed0907be1ea28eee53dbb8ca
thiagoxavier <xavier7132@gmail.com>
2020-04-06 17:34:57 -0300

    fix path

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main.java.launch;
+package launch;
 
 import java.io.File;
 import java.net.URISyntaxException;
